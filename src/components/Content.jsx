43ad1b2fa5a86cf4b202c5db2129f416396990b9
Phuc <duyphuclenguyen@gmail.com>
2024-11-21 13:48:56 -0500

    Add dropdown button for courses

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import Part from './Part'
+import { useState } from 'react'
 
 const Content = ({ parts }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
   return (
     <div>
-      {parts.map((part) => (
-        <Part key={part.id} part={part.name} link = {part.link}/>
-      ))}
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>Bấm vào nếu muốn não bạn to ra</button>
+      </div>
+      <div style={showWhenVisible}>
+        {parts.map((part) => (
+          <Part key={part.id} part={part.name} link = {part.link}/>
+        ))}
+        <button onClick={toggleVisibility}>Ẩn vào nếu bạn muốn ngu đi</button>
+      </div>
     </div>
   )
 }
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react'
 import React from 'react'
 import Content from './Content'
 import Header from './Header'
 
 const Course = ({ courses }) => {
-    return (
-      <div>
-        {courses.map((course) => (
-          <div key={course.id}>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-          </div>
-        ))}
-      </div>
-    )
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  return (
+    <div>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Course
